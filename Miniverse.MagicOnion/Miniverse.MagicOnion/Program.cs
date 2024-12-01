28605430932f6f774d2d55e62824589a3458f631
Euglenach <midorimushiiii@gmail.com>
2024-12-02 02:58:06 +0900

    MagicOnionのホスティングとZLoggerの設定

--- Miniverse.MagicOnion/Miniverse.MagicOnion/Program.cs
+++ Miniverse.MagicOnion/Miniverse.MagicOnion/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// https://github.com/Cysharp/MagicOnion?tab=readme-ov-file#quick-start
 
-Console.WriteLine("Hello, World!");
+using MagicOnion;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ZLogger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddMagicOnion();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddZLoggerConsole().SetMinimumLevel(LogLevel.Debug);
+
+var app = builder.Build();
+
+app.MapMagicOnionService();
+
+app.Run();
