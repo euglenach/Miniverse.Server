b3e54a441095e361c6f2746de255438e7a5270c5
Euglenach <midorimushiiii@gmail.com>
2024-12-03 06:08:19 +0900

    Matching用のStreamingHubを定義

---
+++ Miniverse.MagicOnion/Miniverse.MagicOnion/StreamingHubs/MatchingHub.cs
@@ -0,0 +1,18 @@
+﻿using MagicOnion.Server.Hubs;
+using Microsoft.Extensions.Logging;
+using MiniverseShared.MessagePackObjects;
+using MiniverseShared.StreamingHubs;
+using ZLogger;
+
+namespace Miniverse.MagicOnion.StreamingHubs;
+
+public class MatchingHub(ILogger logger) : StreamingHubBase<IMatchingHub, IMatchingReceiver>, IMatchingHub
+{
+    private Player player;
+    
+    public async ValueTask JoinAsync(Player player, Ulid roomUlid)
+    {
+        this.player = player;
+        logger.ZLogInformation($"Joining matching hub... RoomUlid:{roomUlid} Player:{player.Ulid}");
+    }
+}
