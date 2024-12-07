0e02e92148fc5017e500d09c52e003c46baed7d7
Euglenach <midorimushiiii@gmail.com>
2024-12-08 08:17:08 +0900

    こめけし

--- Miniverse.LogicLooper/Miniverse.LogicLooper/NatsReceiver.cs
+++ Miniverse.LogicLooper/Miniverse.LogicLooper/NatsReceiver.cs
@@ -8,11 +8,6 @@ public class NatsReceiver(NatsPubSub nats, ILogger<NatsPubSub> logger)
 {
     public async ValueTask StartSubscribe()
     {
-        // Subscribe on one terminal
-        // await foreach (var msg in nats.Subscribe<CreateRoomMsg>())
-        // {
-        //     logger.ZLogInformation($"Received: {msg}");
-        // }
         await foreach (var msg in nats.Subscribe<CreateRoomMsg>())
         {
             logger.ZLogInformation($"Received: {msg}");
