47963ff252fcc2abed25aa9bddd6ea6fdfb77552
Euglenach <midorimushiiii@gmail.com>
2024-12-12 14:45:11 +0900

    extension

---
+++ Miniverse.LogicLooper/Miniverse.LogicLooper/Utility/LooperTask/LogicLooperActionContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading;
+
+namespace Miniverse.LogicLooper.LooperTasks;
+
+public static class LogicLooperActionContextExtensions
+{
+    public static LooperTask CreateLooperTask(this LogicLooperActionContext context, LooperHelper? looperHelper = null)
+    {
+        looperHelper ??= new LooperHelper();
+        var looperTask = new LooperTask(looperHelper);
+        context.Looper.RegisterActionAsync(static (in LogicLooperActionContext ctx, (LooperHelper, LogicLooperActionContext) state) =>
+        {
+            var (looperHelper, originContext) = state;
+            if(originContext.CancellationToken.IsCancellationRequested) return false;
+            if(ctx.CancellationToken.IsCancellationRequested) return false;
+            
+            looperHelper.Update(ctx);
+            return true;
+        }, (looperHelper, context));
+
+        return looperTask;
+    }
+}
